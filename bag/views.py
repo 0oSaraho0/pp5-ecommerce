215ee7b27e67aff4621924f0bb8aef31387b84dc
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-02 10:07:40 +0000

    add delete item functionality and fix bug on remove url

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 
@@ -8,7 +8,7 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, item_bag_id):
+def add_to_bag(request, bag_item_id):
     """ Add  item to bag """
 
     quantity = 1
@@ -16,11 +16,22 @@ def add_to_bag(request, item_bag_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_bag_id in list(bag.keys()):
+    if bag_item_id in list(bag.keys()):
         messages.error(request, "The requested quantity is not available")
     else:
-        bag[item_bag_id] = quantity
+        bag[bag_item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, bag_item_id):
+    """ Remove item from bag """
+    
+    bag = request.session.get('bag', {})
+    bag.pop(bag_item_id)
+
+    request.session['bag'] = bag
+    messages.success(request, "{{item.item.name}} has been successfully removed")
+    return redirect(reverse('view_bag'))
+
