78ede8ece8e83313b8e12b6df2c9829cbcbfd564
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-06 11:58:25 +0000

    add checkout views and templates

--- laneys_loft/settings.py
+++ laneys_loft/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'items',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'laneys_loft.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -71,11 +74,16 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- laneys_loft/urls.py
+++ laneys_loft/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('items/', include('items.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
