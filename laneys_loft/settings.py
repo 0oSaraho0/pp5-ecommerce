9efd104aa559a54b36c84e59e195138294e74880
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-12 11:09:02 +0000

    remove secret key

--- laneys_loft/settings.py
+++ laneys_loft/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0s08j@85lde&#6*g#crq%77ly*jjd+$f*q58rsnq_9hajzw9qi'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
