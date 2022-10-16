1631658078924235611d126ebefe52f6d1a09a46
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-16 14:37:46 +0000

    add blog post admin

---
+++ blog_posts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import BlogPost
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(BlogPost)
+class BlogIdeaAdmin(SummernoteModelAdmin):
+    """ A class to display idea ideams on admin """
+
+    list_display = ('event_name', 'created_on')
+    search_fields = ['event_name', 'charity_name', 'content']
+    list_filter = ('created_on',)
+    summernote_fields = ('content')
