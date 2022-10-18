88fbbdf45fbe8471eed0c9a22982bd9173935117
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-18 18:36:32 +0000

    add create blog post url

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('blog_posts/', views.BlogPostList.as_view(), name='blog_posts'),
     path('<int:id>/', views.BlogPostDetailView.as_view(), name='blog_post_detail'),
+    path('create/', views.BlogPostCreateView.as_view(), name='create_blog_post'),
 
 
 
