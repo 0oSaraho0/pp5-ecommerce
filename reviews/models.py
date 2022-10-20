40a8f0691c9a72ffc2bcf70c4a6a1aeb56f57721
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-20 14:32:19 +0000

    add review model

---
+++ reviews/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Review(models.Model):
+    """ A model to create a blog post """
+    title = models.CharField(max_length=200, null=True)
+    user = models.CharField(max_length=200)
+    review = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """ Order by created on date """
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.review} by {self.user}"
