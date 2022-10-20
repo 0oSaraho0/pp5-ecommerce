39e9d42f37a8794e605dde963bbbdb75b9927284
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-20 14:30:16 +0000

    add reviews base url

--- laneys_loft/urls.py
+++ laneys_loft/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
     path('blog_posts/', include('blog_posts.urls')),
+    path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
