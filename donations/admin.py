997fce8e08b87d477cd3f2ac53398975c7162bb6
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-21 15:16:58 +0000

    Register Donation model in admin

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    """ A class to display Donations requests on admin """
+
+    list_display = ('full_name', 'donation_description', 'number_of_bags')
+    search_fields = ['full_name', 'number_of_bags']
+    list_filter = ('full_name', 'number_of_bags')
