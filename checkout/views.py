fc67ad86df1d362b17f9e01cb232fd71552264e7
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-23 14:19:37 +0000

    Make corrections according to pep8 checker

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -13,6 +14,7 @@ from bag.contexts import bag_contents
 
 import stripe
 import json
+
 
 @require_POST
 def cache_checkout_data(request):
@@ -32,7 +34,7 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
-    
+
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -55,30 +57,33 @@ def checkout(request):
             order = order_form.save()
             for bag_item_id, item_data in bag.items():
                 try:
-                    item = Item.objects.get(sku=bag_item_id)                   
+                    item = Item.objects.get(sku=bag_item_id)
                     order_line_item = OrderLineItem(
                         order=order,
                         item=item,
                     )
                     order_line_item.save()
-                    
+
                 except Item.DoesNotExist:
                     messages.error(request, (
-                        "One of the items in your bag wasn't found in our database. "
+                        "One of the items in your bag"
+                        "wasn't found in our database."
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse(
+                'checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(
+                request, "There's nothing in your bag at the moment")
             return redirect(reverse('items'))
 
         current_bag = bag_contents(request)
@@ -90,7 +95,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form
+        # with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
@@ -139,14 +145,14 @@ def checkout_success(request, order_number):
 
         # Save the users info
         if save_info:
-            profile_data = {        
+            profile_data = {
                 'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
                 'default_town_or_city': order.town_or_city,
                 'default_street_address1': order.street_address1,
                 'default_street_address2': order.street_address2,
-                'default_county': order.county, 
+                'default_county': order.county,
             }
 
             user_profile_form = UserProfileForm(profile_data, instance=profile)
@@ -157,7 +163,17 @@ def checkout_success(request, order_number):
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
+    # item_quantity = get_object_or_404(Item,)
+    # bag = request.session.get('bag', {})
+    # for bag_item_id, item_data in bag.items():
+    #     item = Item.objects.get(sku=bag_item_id)
+    #     item_quantity = item_quantity - item
+
+    #     item_quantity.save()
+    # remove bag from checkout bag
+
     if 'bag' in request.session:
+
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
