d050bdab37029bd8e436d57b426b596c114bcd18
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-23 09:08:22 +0000

    add subscribe template, url and view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 
-
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def Subscribe(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/subscribe.html')
