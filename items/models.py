df0a5718431fa72466017c675994de48533ec730
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-23 13:00:55 +0000

    Add quantity field to items model

--- items/models.py
+++ items/models.py
@@ -76,6 +76,8 @@ class Item(models.Model):
     quality = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+
     
 
     def __str__(self):
