2055c59d615feade8bbe658c2393e958d376feae
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-25 18:44:10 +0000

    Update for pep8 check

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -3,14 +3,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog_posts/', views.BlogPostList.as_view(), name='blog_posts'),
-    path('<int:id>/', views.BlogPostDetailView.as_view(), name='blog_post_detail'),
-    path('create/', views.BlogPostCreateView.as_view(), name='create_blog_post'),
-    path('edit/<int:id>/', views.EditBlogPostView.as_view(), name='edit_blog_post'),
-    path('delete/<int:id>/', views.BlogPostDelete.as_view(), name='blog_post_delete'),
-
-
-
-
-    
+    path(
+        'blog_posts/',
+        views.BlogPostList.as_view(),
+        name='blog_posts'
+    ),
+    path(
+        '<int:id>/',
+        views.BlogPostDetailView.as_view(),
+        name='blog_post_detail'
+    ),
+    path(
+        'create/',
+        views.BlogPostCreateView.as_view(),
+        name='create_blog_post'
+    ),
+    path(
+        'edit/<int:id>/',
+        views.EditBlogPostView.as_view(),
+        name='edit_blog_post'
+    ),
+    path(
+        'delete/<int:id>/',
+        views.BlogPostDelete.as_view(),
+        name='blog_post_delete'
+    ),
 ]
