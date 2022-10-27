3e96c8b8ecebe2cc962c7bd107ab0837c5738acc
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-10-27 09:00:21 +0000

    Big fix on blog post pic and update styling

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     charity_name = models.CharField(max_length=200)
     event_name = models.CharField(max_length=200)
     donation_amount = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default='placeholder')
     summary = models.TextField(blank=True)
     charity_location = models.CharField(max_length=200, unique=False)
     charity_website = models.URLField(
